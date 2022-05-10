4d09d56d5c13708820df4fe24ace89145f8a5c4d
Kristy Nguyen <knyen008@gmail.com>
2022-05-10 10:31:27 -0600

    Refactor initialize and countCards tests

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -4,23 +4,31 @@ const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 
 describe('Deck', () => {
+  let card1,
+      card2,
+      card3,
+      deck;
+  beforeEach(() => {
+    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+
+    deck = new Deck([card1, card2, card3]);
+  });
 
   it('should be a function', () => {
-    expect(Deck).to.be.a('function')
+    expect(Deck).to.be.a('function');
   });
 
   it('should be an instance of Deck', () => {
-    const deck = new Deck();
     expect(deck).to.be.an.instanceof(Deck);
   });
 
+  it('should be initialized with an array of Card objects', () => {
+    expect(deck.cards).to.deep.equal([card1, card2, card3]);
+  });
+
   it('should be able to count how many cards are in a deck', () => {
-    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-
-    const deck = new Deck([card1, card2, card3]);
-
     deck.countCards();
 
     expect(deck.countCards()).to.equal(3);
