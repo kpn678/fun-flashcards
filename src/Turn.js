525ef6ea15690c722188971e72285ef627f9d21a
Kristy Nguyen <knyen008@gmail.com>
2022-05-09 19:37:36 -0600

    Test evaluateGuess and giveFeedback methods

--- src/Turn.js
+++ src/Turn.js
@@ -9,6 +9,20 @@ class Turn {
   returnCard() {
     return this.card;
   }
+  evaluateGuess() {
+    if (this.guess === this.card.correctAnswer) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  giveFeedback() {
+    if (this.guess === this.card.correctAnswer) {
+      return 'correct!';
+    } else {
+      return 'incorrect!';
+    }
+  }
 };
 
 module.exports = Turn;
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -7,7 +7,7 @@ describe('Turn', () => {
   let turn;
   let card;
   beforeEach(() => {
-    turn = new Turn('object', card);
+    turn = new Turn('array', card);
     card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
   });
 
@@ -20,15 +20,25 @@ describe('Turn', () => {
   });
 
   it('should have arguments for a string guess and Card object', () => {
-    expect(turn.guess).to.equal('object');
+    expect(turn.guess).to.equal('array');
     expect(turn.card).to.deep.equal(card);
   });
 
   it('should return the guess', () => {
-    expect(turn.returnGuess()).to.equal('object');
+    expect(turn.returnGuess()).to.equal('array');
   });
 
   it('should return the card', () => {
     expect(turn.returnCard()).to.deep.equal(card);
   });
+
+  it('should return boolean depending on if user guess matches correct answer on card', () => {
+    turn.evaluateGuess();
+    expect(turn.evaluateGuess()).to.equal(false);
+  });
+
+  it('should return incorrect or correct depending on if user guess matches correct answer on card', () => {
+    turn.giveFeedback();
+    expect(turn.giveFeedback()).to.equal('incorrect!');
+  })
 });
