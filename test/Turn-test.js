2bd6f3e5ce9d3c53d3639136b5c0e1aec25db457
Kristy Nguyen <knyen008@gmail.com>
2022-05-09 18:49:55 -0600

    Test whether is is function and is instance of Turn

---
+++ test/Turn-test.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+
+describe('Turn', () => {
+  let turn;
+  let card;
+  beforeEach(() => {
+    turn = new Turn('object', card);
+    card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  });
+
+  it('should be a function', () => {
+    expect(Turn).to.be.a('function')
+  });
+
+  it('should be an instance of Turn', () => {
+    expect(turn).to.be.an.instanceof(Turn);
+  })
+});
