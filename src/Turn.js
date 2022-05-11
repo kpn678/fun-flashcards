891cb6abf341b85ef56ad4c0dcb07d11fcc7f36b
Kristy Nguyen <knyen008@gmail.com>
2022-05-10 22:28:37 -0600

    Refactor evaluateGuess method

--- src/Turn.js
+++ src/Turn.js
@@ -2,26 +2,23 @@ class Turn {
   constructor(guess, card) {
     this.guess = guess;
     this.card = card;
-  }
+  };
   returnGuess() {
     return this.guess;
-  }
+  };
   returnCard() {
     return this.card;
-  }
+  };
   evaluateGuess() {
-    if (this.guess === this.card.correctAnswer) {
-      return true;
-    }
-    return false;
-  }
+    return this.guess === this.card.correctAnswer;
+  };
   giveFeedback() {
     if (this.evaluateGuess()) {
       return 'correct!';
     } else {
       return 'incorrect!';
-    }
-  }
+    };
+  };
 };
 
 module.exports = Turn;
